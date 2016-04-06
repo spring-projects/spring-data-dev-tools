3fa7bda4d1b8ef2960beea5154d46b8abb3d2e91
Oliver Gierke <info@olivergierke.de>
2016-04-06 22:04:31 +0200

    #25 - Added Ingalls to release train model.

--- release-tools/src/main/java/org/springframework/data/release/model/ReleaseTrains.java
+++ release-tools/src/main/java/org/springframework/data/release/model/ReleaseTrains.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class ReleaseTrains {
 
 	public static final List<Train> TRAINS;
-	public static final Train CODD, DIJKSTRA, EVANS, FOWLER, GOSLING, HOPPER;
+	public static final Train CODD, DIJKSTRA, EVANS, FOWLER, GOSLING, HOPPER, INGALLS;
 
 	static {
 
@@ -38,10 +38,11 @@ public class ReleaseTrains {
 		GOSLING = FOWLER.next("Gosling", Transition.MINOR, new Module(KEY_VALUE, "1.0"));
 		HOPPER = GOSLING.next("Hopper", Transition.MINOR, new Module(SOLR, "2.0"), new Module(ENVERS, "1.0"),
 				new Module(NEO4J, "4.1"), new Module(COUCHBASE, "2.1"), new Module(ELASTICSEARCH, "2.0"));
+		INGALLS = HOPPER.next("Ingalls", Transition.MINOR);
 
 		// Trains
 
-		TRAINS = Arrays.asList(CODD, DIJKSTRA, EVANS, FOWLER, GOSLING, HOPPER);
+		TRAINS = Arrays.asList(CODD, DIJKSTRA, EVANS, FOWLER, GOSLING, HOPPER, INGALLS);
 
 		// Train names
 
